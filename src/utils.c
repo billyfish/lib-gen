1074d15b49af79933f7273fa3ba1215fd563afa7
billyfish <billyfish@7c5812b9-91a1-4bf5-a23d-95ff59d0b40a>
2013-06-24 09:23:50 +0000

    git-svn-id: https://svn.riouxsvn.com/libgen@9 7c5812b9-91a1-4bf5-a23d-95ff59d0b40a

--- src/utils.c
+++ src/utils.c
@@ -17,7 +17,7 @@
 char *CopyToNewString (const char *start_p, const char *end_p, const BOOL trim_flag)
 {
 	if (trim_flag)
-		{			
+		{
 			BOOL loop_flag = TRUE;
 
 			while (loop_flag)
@@ -65,25 +65,44 @@ char *CopyToNewString (const char *start_p, const char *end_p, const BOOL trim_f
 	return NULL;
 }
 
+static const char PeekNextChar (const char *text_s, BOOL ignore_whitespace_flag);
+
+static const char PeekNextChar (const char *text_s, BOOL ignore_whitespace_flag)
+{
+	if (*text_s != '\0)
+		{
+			if (ignore_whitespace_flag)
+				{
+					while (isspace (*text_s))
+						{
+							++ text_s;
+						}
+				}
+			else
+				{
+					++ text_s;
+				}
+		}
+
+	return *text_s;
+}
+
 
 /*
+	<type> <function> (<type> <name>, ....);
 
- <type> <function> (<type> <name>, ....);
- 
- function can have dereferences before it thast should be stored 
- as part of the type. Param Types can be function pointers when 
- 
+ function can have dereferences before it thast should be stored
+ as part of the type. Param Types can be function pointers when
+
  first bracket separates the function from its parameters.
  Each string should be trimmed of whitepace where possible.
- 
- 
+
+
  1. find opening bracket and split the string on it.
  2. find the start and end of the function name
- 
- 
+
+
 */
-
-
 struct List *TokenizeFunctionPrototype (const char *prototype_s)
 {
 	struct List *tokens_p = AllocateStringList ();
@@ -91,43 +110,43 @@ struct List *TokenizeFunctionPrototype (const char *prototype_s)
 	if (tokens_p)
 		{
 			const char *opening_bracket_p = strchr (prototype_s, '(');
-			
+
 			if (opening_bracket_p)
 				{
 					char *function_name_s = NULL;
 					char *function_type_s = NULL;
-					
+
 					/* scroll to the end of the function name */
 					const char *name_end_p = ScrollPastWhitespace (opening_bracket_p, prototype_s, NULL, TRUE);
-					
+
 					if (name_end_p)
 						{
 							/* scroll to the start of the function name */
 							const char * const delimiters_s = "*[]";
 							const char *name_start_p = ScrollPastWhitespace (opening_bracket_p, prototype_s, delimiters_s, FALSE);
-							
+
 							if (name_start_p)
 								{
 									function_name_s = CopyToNewString (name_start_p, name_end_p, FALSE);
-									
+
 									if (function_name_s)
 										{
 											function_type_s = CopyToNewString (prototype_s, name_start_p - 1, TRUE);
-											
+
 											if (function_type_s)
 												{
-												
-												}										
+
+												}
 										}
-															
-								}		/* if (name_end_p) */							
-															
+
+								}		/* if (name_end_p) */
+
 						}		/* if (name_end_p) */
-										
+
 				}		/* if (opening_bracket_p) */
-			
+
 		}
-	
+
 	return NULL;
 }
 
@@ -150,12 +169,12 @@ const char *ScrollPastWhitespace (const char *text_p, const char * const bounds_
 				}
 			else if (space_flag == (isspace (*text_p) != 0))
 				{
-					text_p += inc; 
+					text_p += inc;
 				}
 			else if ((delimiters_s != NULL) && (strchr (delimiters_s, *text_p) != NULL))
 				{
 					res_p = text_p - inc;
-					loop_flag = FALSE;					
+					loop_flag = FALSE;
 				}
 			else
 				{
