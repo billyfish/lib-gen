5ad81f9a5f88e18a7449316dbd7807c832b501e9
billyfish <billyfish@7c5812b9-91a1-4bf5-a23d-95ff59d0b40a>
2014-09-06 23:49:57 +0000

    git-svn-id: https://svn.riouxsvn.com/libgen@21 7c5812b9-91a1-4bf5-a23d-95ff59d0b40a

---
+++ src/idl.c
@@ -0,0 +1,82 @@
+
+
+BOOL WriteIDLHeader (FILE *out_f, const char * const name_s, const char * const basename_s, const char * const name_s, const char * const struct_name_s, const char * const prefix_s)
+{
+	BOOL success_flag = FALSE;
+
+	if (fprintf (out_f, "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n") >= 0)
+		{
+			if (fprintf (out_f, "<!DOCTYPE library SYSTEM \"library.dtd\">\n") >= 0)
+				{
+					if (fprintf (out_f, "<library name=\"%s\" basename=\"%s\">\n", name_s, basename_s) >= 0)
+						{
+							if (fprintf (out_f, "<interface name=\"%s\" version=\"1\" flags=\"protected\" struct=\"%s\" prefix=\"%s\">\n", name_s, struct_name_s, prefix_s) >= 0)
+								{
+									success_flag = TRUE;
+								}
+						}
+				}
+		}
+
+	return success_flag;
+}
+
+
+BOOL WriteIDLFunction (FILE *out_f, const struct FunctionDefinition * const fd_p)
+{
+	BOOL success_flag = FALSE;
+
+	if (fprintf (out_f, "\t<method name=\"%s\" result=\"%s\">\n", fd_p -> fd_definition_p -> pa_name_s, fd_p -> fd_definition_p -> pa_type_s) >= 0)
+		{
+			ParameterNode *node_p = (ParameterNode *) GET_HEAD (fd_p -> fd_args_p);
+
+			success_flag = TRUE;
+
+			while (success_flag)
+				{
+
+				}
+
+		}
+
+	return success_flag;
+}
+
+
+BOOL WriteIDLDefaultFunctions (FILE *out_f)
+{
+	BOOL success_flag = FALSE;
+
+	if (fprintf (out_f, "\t<method name=\"Obtain\" result=\"uint32\"></method>\n") >= 0)
+		{
+			if (fprintf (out_f, "\t<method name=\"Release\" result=\"uint32\"></method>\n") >= 0)
+				{
+					if (fprintf (out_f, "\t<method name=\"Expunge\" result=\"void\" status=\"unimplemented\"></method>\n") >= 0)
+						{
+							if (fprintf (out_f, "\t<method name=\"Clone\" result=\"struct Interface *\" status=\"unimplemented\"></method>\n") >= 0)
+								{
+									success_flag = TRUE;
+								}
+						}
+				}
+		}
+
+	return success_flag;
+}
+
+
+
+BOOL WriteIDLIncludes (FILE *out_f, const char * const name_s, const char * const basename_s, const char * const name_s, const char * const struct_name_s, const char * const prefix_s)
+{
+	BOOL success_flag = TRUE;
+
+	return success_flag;
+}
+
+
+
+
+BOOL WriteIDLFooter (FILE *out_f)
+{
+	return (fprintf (out_f, "</library>\n") >= 0);
+}
