719945324725b12766751f0ca8d0f8af9e80e477
billyfish <billyfish@7c5812b9-91a1-4bf5-a23d-95ff59d0b40a>
2014-11-23 23:26:22 +0000

    git-svn-id: https://svn.riouxsvn.com/libgen@91 7c5812b9-91a1-4bf5-a23d-95ff59d0b40a

--- src/document_parser.c
+++ src/document_parser.c
@@ -14,7 +14,40 @@ STRPTR StripComments (struct DocumentParser *parser_p, STRPTR line_p)
 	/* are we already in a comment? */
 	if (parser_p -> dp_comment_flag)
 		{
+			STRPTR comment_end_p = strstr (line_p, "*/");
 
+			if (comment_end_p)
+				{
+					/* Is there a new starting comment? */
+					STRPTR c_comment_p = strstr (comment_end_p, "/*");
+					STRPTR cpp_comment_p = strstr (comment_end_p, "//");
+					STRPTR comment_start_p = NULL;
+
+					if (c_comment_p)
+						{
+							if (cpp_comment_p)
+								{
+									comment_start_p = (c_comment_p < cpp_comment_p) ? c_comment_p : cpp_comment_p;
+								}
+							else
+								{
+									comment_start_p = c_comment_p;
+								}
+						}
+					else
+						{
+							comment_start_p = cpp_comment_p;
+						}
+
+					if (comment_start_p)
+						{
+						}
+
+				}
+			else
+				{
+					/* still in comment */
+				}
 		}
 
 }
