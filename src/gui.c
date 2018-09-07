d45f75cddd9e63a37083ea7ce4771952d705c408
billy <tyrrell.simon@gmail.com>
2018-09-08 00:44:06 +0100

    adding initial gui files

---
+++ src/gui.c
@@ -0,0 +1,30 @@
+/*
+
+************************************************************
+**
+** Created by: codebench 0.55 (19.10.2017)
+**
+** Project: libgen
+**
+** File: gui.c
+**
+** Date: 08-09-2018 00:32:53
+**
+************************************************************
+
+*/
+
+#include "gui.h"
+
+
+
+
+BOOL OpenPrefsGUI (LibGenPrefs *prefs_p)
+{
+	BOOL success_flag = FALSE;
+
+	Object *window_p
+	
+	
+	return success_flag;
+}
---
+++ src/gui.h
@@ -0,0 +1,29 @@
+/*
+
+************************************************************
+**
+** Created by: codebench 0.55 (19.10.2017)
+**
+** Project: libgen
+**
+** File: gui.h
+**
+** Date: 08-09-2018 00:33:14
+**
+************************************************************
+
+*/
+
+
+#ifndef GUI_H
+#define GUI_H
+
+#include <exec/types.h>
+
+#include "prefs.h"
+
+
+BOOL OpenPrefsGUI (LibGenPrefs *prefs_p); 
+
+
+#endif
